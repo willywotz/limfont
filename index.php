70a6c9c69219af70b85e0e250827e3c034a414b8
willywotz <dogajack@gmail.com>
2023-07-16 17:18:44 +0700

    add index template

---
+++ index.php
@@ -0,0 +1,79 @@
+<?php
+
+include 'ubn.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>KOREA.SHOP</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="d-flex align-items-center">
+      <div class="w-100 m-auto py-4" style="max-width: 800px">
+        <div>k. <?=user()->displayName; ?></div>
+
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">วันที่สั่งซื้อสินค้า</th>
+              <th scope="col">ชื่อสินค้า</th>
+              <th scope="col">วิธีชำระเงิน</th>
+              <th scope="col">รหัสสินค้า</th>
+              <th scope="col">สถานะสินค้า</th>
+              <th scope="col">แจ้งเลขพัสดุ</th>
+              <th scope="col">ที่อยู่ / เปลี่ยนที่อยู่</th>
+              <th scope="col">สินค้าในสต๊อก</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+              <td>@mdo</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div class="text-center">
+          <a href="logout.php" class="btn btn-secondary" style="width: 150px">ออก</a>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </body>
+</html>
