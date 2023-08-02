ca3f27d29208af432bd0bff2c473e1060ce4dc17
willywotz <dogajack@gmail.com>
2023-08-03 02:15:55 +0700

    add navbar

--- index.php
+++ index.php
@@ -64,10 +64,21 @@ app();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
     <style>
     * { box-sizing: border-box; }
     html, body { font: 16px/1.5 sans-serif; color: #333; }
     body { margin: 0; }
+
+    <?php if (app()->page != 'login'): ?>
+    .navbar { margin: 0 auto; max-width: 600px; }
+    .navbar-body { display: flex; }
+    .navbar-body h1, .navbar-body span { padding: 1rem; }
+    .navbar-body h1 { margin: 0; }
+    .navbar-title { flex: 1; background-color: hsla(0deg 0% 0% / 25%); }
+    .navbar-link { background-color: hsla(0deg 0% 0% / 75%); color: #fff; display: flex; align-items: center; padding: 0 1rem; text-decoration: none; }
+    @media screen and (min-width: 800px) { .navbar-body { margin-top: 4rem; } }
+    <?php endif; ?>
 
     <?php if (app()->page == 'login'): ?>
     .login-block { position: fixed; left: 0; right: 0; top: 0; bottom: 0; }
@@ -83,6 +94,25 @@ app();
     </style>
   </head>
   <body>
+    <?php if (app()->page != 'login'): ?>
+    <div class="navbar">
+      <div class="navbar-body">
+        <a href="index.php?p=home" class="navbar-title">
+          <h1>KOREA.SHOP</h1>
+        </a>
+        <?php if (app()->user): ?>
+        <a href="index.php?p=profile" class="navbar-link">
+          <span class="material-symbols-outlined">account_circle</span>
+        </a>
+        <?php else: ?>
+        <a href="index.php?p=login" class="navbar-link">
+          <span class="material-symbols-outlined">login</span>
+        </a>
+        <?php endif; ?>
+      </div>
+    </div>
+    <?php endif; ?>
+
     <?php if (app()->page == 'login'): ?>
     <div class="login-block">
       <form class="login-body" action="index.php?p=login" method="POST">
