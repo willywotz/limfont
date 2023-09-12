67ba06a02562f7b3429ffe05fce35083026b1b9f
willywotz <dogajack@gmail.com>
2023-09-12 23:16:45 +0700

    admin.account.index

--- admin.account.index.php
+++ admin.account.index.php
@@ -34,5 +34,17 @@
     </div>
 </div>
 
-<div></div>
+<div class="mt-8 grid grid-cols-3 gap-4">
+    <?php $accounts = db()->query('select * from account')->fetchAll(PDO::FETCH_OBJ); ?>
+    <?php foreach ($accounts as $account): ?>
+    <div class="rounded bg-white overflow-hidden">
+        <div class="py-2 px-4"><?= $account->type == '1' ? 'รายรับ' : ($account->type == '2' ? 'รายจ่าย' : '') ?></div>
+        <div class="pb-4 px-4 text-2xl"><?= $account->amount ?></div>
+        <div class="grid grid-cols-2 text-center">
+            <a href="admin.account.update.php?id=<?= $account->id ?>" class="py-2 bg-yellow-500 hover:bg-yellow-700">แก้ไข</a>
+            <a href="admin.account.delete.php?id=<?= $account->id ?>" class="py-2 bg-red-500 hover:bg-red-700">ลบ</a>
+        </div>
+    </div>
+    <?php endforeach ?>
+</div>
 <?php include '_foot.php' ?>
